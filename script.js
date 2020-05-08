e091d4eaed2a9de53b847f8d51bd2ca135505033
Tee Coder <tydangelo18@gmail.com>
2020-05-08 17:02:45 -0500

    Worked on Javascript: pushed prompt criteria to an array, used Math.Random to generate password.

--- script.js
+++ script.js
@@ -1,55 +1,93 @@
-let generateBtn = document.querySelector("#generate")
+let generateBtn = document.querySelector("#generate") //selects button element to generate a password when clicked.
 
-let specialCharacters = '~`!@#$%^&*()-_=+""{[}]\|:;<,>./?'
+let generateBox = document.querySelector("#password")
 
-let lowerLetters = 'qwertyuiopasdfghjklzxcvbnm'
+ //add an event listener to button element.
 
-let upperLetters = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+let specialCharacters = '~`!@#$%^&*()-_=+""{[}]\|:;<,>./?'; //global variable
 
-let digits = '1234567890'
+let lowerLetters = 'qwertyuiopasdfghjklzxcvbnm'; //global variable
 
-function writePassword() {
-    let passwordLength = parseInt(prompt("Between 8 and 128 characters, how many characters would you like your password to be?"));
+let upperLetters = 'QWERTYUIOPASDFGHJKLZXCVBNM'; //global variable
 
+let digits = '0123456789'; //global variable
+
+let finalPassword = []; // create an empty array to add results to based on user prompts. 
+
+
+// Receive Password Criteria from the user. 
+
+function createPassword() {
+    
+    let passwordLength = prompt("Between 8 and 128 characters, how many characters would you like your password to be?");
+    
     if (passwordLength < 8 || passwordLength > 128) {
         alert("Please choose between 8 and 128 characters.")
-        return writeLength();
+        return createPassword();
         
+    }  
+
+    // Add in if statements for user to see which cirteria they've selected. 
+
+    let characterLower = window.confirm("Do you want lowercase characters? (OK = Yes, Cancel = No)");
+
+
+    let characterUpper = window.confirm("Do you want uppercase characters? (OK = Yes, Cancel = No)");
+
+    
+    let characterSpecial = window.confirm("Do you want special characters? (OK = Yes, Cancel = No)");
+
+    
+    let characterNumber = window.confirm("Do you want numbers? (OK = Yes, Cancel = No)");
+    
+
+    
+    //Add selected user password criteria into finalPassword Array. 
+
+    if (passwordLength >=8) {
+        finalPassword.push(...finalPassword);
     }
-    let characterLower = window.confirm("Do you want lowercase characters?"); (true);
 
-    if (confirm(characterLower)) {
-        window.alert("Okay, lowercase added!")
-        
-    } else {
-        window.alert("Okay, no lowercase.")
-        
+    if (characterLower === true) {
+        finalPassword.push(...lowerLetters);
     }
 
-    let characterUpper = window.confirm("Do you want uppercase characters?"); (true);
-
-    if (confirm(characterUpper)) {
-        window.alert("Okay uppercase added!")
-
-    } else {
-        window.alert("Okay, no uppercase.")
+    if (characterUpper === true) {
+        finalPassword.push(...upperLetters);
     }
 
-    let characterSpecial = window.confirm("Do you want special characters?"); (true);
+    if (characterSpecial === true) {
+        finalPassword.push(...specialCharacters);
+    }
 
-    if (confirm(characterSpecial)) {
-        window.alert("Okay, special characters added!")
+    if (characterNumber === true) {
+        finalPassword.push(...digits);
+    }
 
-    } else {
-        window.alert("Okay, no special characters.")
+    if (finalPassword.length !=0) {
+        final(finalPassword, passwordLength); //must make sure user selected at least one criteria
     }
 
 }
 
 
+// Generate Random Password using math.random from the pushed array
+
+function final(finalPassword, passwordLength) {
+    function random(array) {
+        array.sort(() => Math.random() -0.5);
+    }
+
+    random(finalPassword);
+    document.getElementById("password").innerHTML = 
+        "Here you go:   " + finalPassword.slice(0, passwordLength).join("");
+}
+
+
+
+generateBtn.addEventListener("click", createPassword); // add an Event Listener to the button element. 
 
 
 
 
-generateBtn.addEventListener("click", writePassword); 
 
